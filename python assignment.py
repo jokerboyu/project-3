f75b0566988043c6ac5ed966fb9e8b09d6a7a8c1
jokerboyu <118470370+jokerboyu@users.noreply.github.com>
2023-02-28 11:46:03 +0200

    Create python assignment.py

---
+++ python assignment.py
@@ -0,0 +1,41 @@
+="guannie"
+c="cystosine"
+a="adenine"
+t="thymine"
+
+DNA1="cystosine"
+DNA2="guannie"
+DNA3="thyrine"
+DNA4="aderine"
+
+if(g==DNA1):
+    {
+
+     print (" G is eqivilanet!")
+
+
+        }
+elif(g==DNA2):
+    {
+
+     print("G is eqivilanet")
+
+
+        }
+
+elif (g==DNA3):
+    {
+
+
+      print ("G is eqivilanet")
+
+        }
+elif (g==DNA4):
+    {
+
+      print ("G is eqivilanet")
+
+        }
+
+else:
+    print ("no match")
