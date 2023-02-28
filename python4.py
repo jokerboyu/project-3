e2a88ad6ee5cde9178afd6cb69bd8cd105481de1
jokerboyu <118470370+jokerboyu@users.noreply.github.com>
2023-02-28 11:57:24 +0200

    Create python4.py

---
+++ python4.py
@@ -0,0 +1,23 @@
+i=1
+while i<6:
+    
+    i+=1
+    print (i)
+    output:
+     2
+     3
+     4
+     5
+     6
+     
+     i=1
+while i<6:
+     print (i)
+    i+=1
+   
+    output:
+    1
+    2
+    3
+    4
+    5
